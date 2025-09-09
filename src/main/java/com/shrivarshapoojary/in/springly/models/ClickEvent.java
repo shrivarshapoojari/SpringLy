e1a83575a4d0d0b9acf4c814c06167c18d3f10e4
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-09-09 17:57:04 +0530

    create clickevent and url mapping classes

--- src/main/java/com/shrivarshapoojary/in/springly/models/ClickEvent.java
+++ src/main/java/com/shrivarshapoojary/in/springly/models/ClickEvent.java
@@ -1,4 +1,24 @@
 package com.shrivarshapoojary.in.springly.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="click_event")
 public class ClickEvent {
+
+    private Long id;
+    private LocalDateTime clickDate;
+
+    @ManyToOne
+    @JoinColumn(name="url_mapping_id")
+    private UrlMapping urlMapping;
+
+
 }
