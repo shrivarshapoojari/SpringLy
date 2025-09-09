0bbea40ce6f249edff825cd7d837ed38687ae36f
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-09-09 16:01:10 +0530

    set up

---
+++ src/main/java/com/shrivarshapoojary/in/springly/SpringlyApplication.java
@@ -0,0 +1,13 @@
+package com.shrivarshapoojary.in.springly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrivarshapoojary/in/springly/SpringlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrivarshapoojary.in.springly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
