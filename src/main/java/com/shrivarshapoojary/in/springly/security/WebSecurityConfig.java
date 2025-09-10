ce049ccc0d27aa9e2dd71b03b08a4ede45f24e00
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-09-10 15:45:15 +0530

    security configs complete

---
+++ src/main/java/com/shrivarshapoojary/in/springly/security/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.shrivarshapoojary.in.springly.security;
+
+import com.shrivarshapoojary.in.springly.security.jwt.JwtAuthenticationFilter;
+import com.shrivarshapoojary.in.springly.service.UserDetailsServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+@AllArgsConstructor
+public class WebSecurityConfig {
+
+     private UserDetailsServiceImpl userDetailsService;
+
+     @Bean
+     public JwtAuthenticationFilter jwtAuthenticationFilter()
+     {
+         return  new JwtAuthenticationFilter();
+     }
+
+     @Bean
+     public PasswordEncoder  passwordEncoder()
+     {
+         return  new BCryptPasswordEncoder();
+     }
+
+     public DaoAuthenticationProvider authenticationProvider(){
+         DaoAuthenticationProvider authProvider=new DaoAuthenticationProvider();
+         authProvider.setUserDetailsService(userDetailsService);
+         authProvider.setPasswordEncoder(passwordEncoder());
+         return authProvider;
+
+     }
+
+     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+         http.csrf(csrf->csrf.disable())
+                 .authorizeHttpRequests(auth->auth
+                         .requestMatchers(("/api/auth/**")).permitAll()
+
+
+                         .requestMatchers(("/api/urls/**")).authenticated()
+                         .requestMatchers(("/{shortUrl")).permitAll()
+                         .anyRequest().authenticated()
+                 );
+
+
+         http.authenticationProvider(authenticationProvider());
+         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+         return  http.build();
+
+     }
+
+}
